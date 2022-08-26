836e52dd38ec32df88a38fb3b0477abb10bc746f
Maru32768 <maru32768@gmail.com>
2022-08-26 19:13:05 +0900

    init

--- src/main/java/net/kunmc/lab/customtnt/CustomTNTPlugin.java
+++ src/main/java/net/kunmc/lab/customtnt/CustomTNTPlugin.java
@@ -1,8 +1,8 @@
-package net.kunmc.lab.paperplugintemplate;
+package net.kunmc.lab.customtnt;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class PaperPluginTemplate extends JavaPlugin {
+public final class CustomTNTPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
     }
