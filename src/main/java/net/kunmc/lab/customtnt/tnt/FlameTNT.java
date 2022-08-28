769340e3821ce0557f7af23508e26892f6359ca8
Maru32768 <maru32768@gmail.com>
2022-08-28 10:39:32 +0900

    fix bug

--- src/main/java/net/kunmc/lab/customtnt/tnt/FlameTNT.java
+++ src/main/java/net/kunmc/lab/customtnt/tnt/FlameTNT.java
@@ -2,6 +2,7 @@ package net.kunmc.lab.customtnt.tnt;
 
 import net.kunmc.lab.customtnt.CustomTNT;
 import net.kunmc.lab.customtnt.config.FlameConfig;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.TNTPrimed;
@@ -29,7 +30,10 @@ public class FlameTNT extends CustomTNT {
         int radius = config.radius.value();
         for (int x = -radius; x <= radius; x++) {
             for (int z = -radius; z <= radius; z++) {
-                Block b = tnt.getLocation().add(x, 0, z).toHighestLocation().add(0, 1, 0).getBlock();
+                Location loc = tnt.getLocation().add(x, 0, z);
+                int y = tnt.getWorld().getHighestBlockYAt(loc);
+                loc.setY(y + 1);
+                Block b = loc.getBlock();
                 b.setType(Material.FIRE);
             }
         }
--- src/main/java/net/kunmc/lab/customtnt/tnt/LGBTQTNT.java
+++ src/main/java/net/kunmc/lab/customtnt/tnt/LGBTQTNT.java
@@ -2,6 +2,7 @@ package net.kunmc.lab.customtnt.tnt;
 
 import net.kunmc.lab.customtnt.CustomTNT;
 import net.kunmc.lab.customtnt.config.LGBTQConfig;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.TNTPrimed;
@@ -43,8 +44,11 @@ public class LGBTQTNT extends CustomTNT {
         int n = 0;
         for (int x = -radius; x <= radius; x++) {
             for (int z = -radius; z < radius; z++) {
-                Block b = tnt.getLocation().add(x, 0, z).toHighestLocation().getBlock();
-                b.setType(colorMaterials.get(n % colorMaterials.size()));
+                Location loc = tnt.getLocation().add(x, 0, z);
+                int y = tnt.getWorld().getHighestBlockYAt(loc);
+                loc.setY(y);
+                Block b = loc.getBlock();
+                b.setType(colorMaterials.get(n % colorMaterials.size()), false);
             }
 
             n++;
